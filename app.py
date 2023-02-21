8ea37a47564b8b94d36961697f77032c62c0fc04
David Rose <david010@gmail.com>
2023-02-21 13:30:03 -0600

    clean up code

--- app.py
+++ app.py
@@ -4,48 +4,9 @@ import gradio as gr
 from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 from langchain.prompts.prompt import PromptTemplate
 
-_TEMPLATE_SQL = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-Use the following format:
+from templates.templates import _TEMPLATE_SQL
 
-Question: "Question here"
-SQLQuery: "SQL Query to run"
-SQLResult: "Result of the SQLQuery"
-Answer: "Final answer here"
 
-Only use the following tables:
-
-{table_info}
-
-If someone asks for the table events, they really mean the crm.events.recs_events table.
-
-Important: all the SQL queries should be run using ONLY Snowflake specific syntax and commands.
-Please make sure all SQL code is specific to Snowflake, and not generic SQL.
-
-Extra information:
-- event_type holds the type of event, such as 'attributed_conversion' or 'conversion' or 'bt_rec_served'
-- bsin - user id
-- use filter site_id = 'citibank' if not specified
-- please use current_date() to calculate the current date, do not assume the year is 2021, or any other year you think it is
-- be sure to use quotes around strings, such as 'citibank' or INTERVAL '2 MONTH'
-- try to avoid nested aggregrations
-- use temp tables, instead of subqueries or nested queries
-- impressions are referred to in the data as bt_rec_served
-- conversion_rate can be calculated by dividing conversion counts by bt_rec_served counts event types
-
-Again: please make sure all code is specific to snowflake
-
-Example:
-SELECT 
-  COUNT(*) AS postimpression_count, 
-  COUNT(CASE WHEN attribution_type = 'POSTCLICK' THEN 1 END) AS postclick_count 
-FROM 
-  crm.events.recs_events 
-WHERE 
-  event_type = 'attributed_conversion' AND site_id = 'citibank';
-
-Begin!
-
-Question: {input}"""
 PROMPT_SQL = PromptTemplate(
     input_variables=["input", "table_info", "dialect"], template=_TEMPLATE_SQL
 )
@@ -104,28 +65,29 @@ def run_query(uri, openai_key, query):
 
 with gr.Blocks() as demo:
     with gr.Tab("Query"):
-        # Account details
-        sf_account = gr.Textbox(label="Snowflake Account")
-        sf_user = gr.Textbox(label="Snowflake User")
-        sf_password = gr.Textbox(label="Snowflake Password")
-        sf_database = gr.Textbox(label="Snowflake Database")
-        sf_schema = gr.Textbox(label="Snowflake Schema")
-        sf_warehouse = gr.Textbox(label="Snowflake Warehouse")
-        sf_uri = gr.Textbox(label="Snowflake URI")
+        with gr.Accordion("See Details"):
+            # Account details
+            sf_account = gr.Textbox(label="Snowflake Account")
+            sf_user = gr.Textbox(label="Snowflake User")
+            sf_password = gr.Textbox(label="Snowflake Password")
+            sf_database = gr.Textbox(label="Snowflake Database")
+            sf_schema = gr.Textbox(label="Snowflake Schema")
+            sf_warehouse = gr.Textbox(label="Snowflake Warehouse")
+            sf_uri = gr.Textbox(label="Snowflake URI")
 
-        openai_key = gr.Textbox(label="OpenAI API Key")
+            openai_key = gr.Textbox(label="OpenAI API Key")
 
-        load_btn = gr.Button("Load Snowflake Details")
-        outputs = [
-            sf_account,
-            sf_user,
-            sf_password,
-            sf_database,
-            sf_schema,
-            sf_warehouse,
-            sf_uri,
-            openai_key,
-        ]
+            load_btn = gr.Button("Load Snowflake Details")
+            outputs = [
+                sf_account,
+                sf_user,
+                sf_password,
+                sf_database,
+                sf_schema,
+                sf_warehouse,
+                sf_uri,
+                openai_key,
+            ]
         load_btn.click(fn=load_sf_account, outputs=outputs)
 
         sf_query = gr.Textbox(label="What is your question?")
